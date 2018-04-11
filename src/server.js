29083f1c8a110ba4221a91590d9d2fc761096992
lessp <ekander.tom@gmail.com>
2018-04-11 17:00:21 +0200

    Switched to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,35 +1,47 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import mongoose from "mongoose";
+
+const DATABASE_CONNECTION = "mongodb://database/cobalt";
+
+mongoose
+  .connect(DATABASE_CONNECTION)
+  .then(() => console.log("connected to db"));
 
 if (!process.env.PORT) {
-  require('dotenv').config()
+  require("dotenv").config();
 }
 
 if (!process.env.PORT) {
-  console.log('[api][port] 7770 set as default')
-  console.log('[api][header] Access-Control-Allow-Origin: * set as default')
+  console.log("[api][port] 7770 set as default");
+  console.log("[api][header] Access-Control-Allow-Origin: * set as default");
 } else {
-  console.log('[api][node] Loaded ENV vars from .env file')
-  console.log(`[api][port] ${process.env.PORT}`)
-  console.log(`[api][header] Access-Control-Allow-Origin: ${process.env.ALLOW_ORIGIN}`)
+  console.log("[api][node] Loaded ENV vars from .env file");
+  console.log(`[api][port] ${process.env.PORT}`);
+  console.log(
+    `[api][header] Access-Control-Allow-Origin: ${process.env.ALLOW_ORIGIN}`
+  );
 }
 
-const app = express()
-const port = process.env.PORT || 7770
-const allowOrigin = process.env.ALLOW_ORIGIN || '*'
+const app = express();
+const port = process.env.PORT || 7770;
+const allowOrigin = process.env.ALLOW_ORIGIN || "*";
 
 app.listen(port, () => {
-  console.log('[api][listen] http://localhost:' + port)
-})
+  console.log("[api][listen] http://localhost:" + port);
+});
 
-app.use(cors({
-  origin: process.env.ALLOW_ORIGIN,
-  credentials: true,
-  allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
-  methods: 'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-}))
+app.use(
+  cors({
+    origin: process.env.ALLOW_ORIGIN,
+    credentials: true,
+    allowedHeaders: "X-Requested-With, Content-Type, Authorization",
+    methods: "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Cobalt API'))
+app.get("/", (req, res) => res.send("Cobalt API"));
