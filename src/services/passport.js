46b8c8cf57a0cac9ee500d5fd08579354f5f149d
lessp <ekander.tom@gmail.com>
2018-04-16 16:36:04 +0200

    Removed code that is no longer used

--- src/services/passport.js
+++ src/services/passport.js
@@ -3,30 +3,6 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const dbActions = require("../db/actions");
 
-// passport.use(
-//   new LocalStrategy(
-//     {
-//       usernameField: "email",
-//       passwordField: "password"
-//     },
-//     (username, password, done) => {
-//       dbActions
-//         .getUserFromEmail(email)
-//         .then(user => {
-//           if (!user) {
-//             return done(null, false, { message: "Incorrect username." });
-//           }
-
-//           return done(null, user);
-//         })
-//         .catch(err => {
-//           console.log(err);
-//           return done(err);
-//         });
-//     }
-//   )
-// );
-
 passport.use(
   new LocalStrategy(
     {
