68f413ba2b28683a40f2d7d2d5be0c8b752a5232
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2018-04-23 15:34:33 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -43,15 +43,15 @@ app.use(passport.session());
 /* Database */
 const decoded_password = encodeURIComponent(process.env.MONGO_INITDB_ROOT_PASSWORD)
 console.log(decoded_password)
-const DATABASE_CONNECTION = `mongodb://${
-  process.env.MONGO_INITDB_ROOT_USERNAME
-}:${decoded_password}@${
-  process.env.MONGO_INITDB_DATABASE
-}`;
+// const DATABASE_CONNECTION = `mongodb://${
+//   process.env.MONGO_INITDB_ROOT_USERNAME
+// }:${decoded_password}@${
+//   process.env.MONGO_INITDB_DATABASE
+// }`;
 
-mongoose.connect(DATABASE_CONNECTION);
+// mongoose.connect(DATABASE_CONNECTION);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 /* Routes */
 app.get("/", (req, res) => res.send("Cobalt API"));
