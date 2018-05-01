dba4dba860ed8af1a955bbb67a5e98614c4b7926
lessp <ekander.tom@gmail.com>
2018-05-01 17:07:57 +0200

    Updates create session route to also save session to DB

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -9,38 +9,68 @@ import { rooms } from "../socket/socket";
 
 import { requireLogin } from "../middleware";
 
+import * as dbActions from "../db/actions";
+
 const WrappedSessionController = socketMethods => (
-  router.post("/", requireLogin, (req, res) => {
+  router.post("/", requireLogin, async (req, res) => {
     const sessionId = shortid.generate();
+    const { name, description, settings } = req.body;
 
-    rooms[sessionId] = {
-      session: socketMethods.getNewSession(sessionId),
-      attendees: new Map(),
-      presentation: {
-        information: {
-          owner: req.user.name,
-          description: "AI in the future."
-        },
-        data: {
-          settings: {
-            isAverage: true,
-            threshold: 35,
-            description: "Faster, slower"
-          },
-          attendees: 0,
-          engagement: {
-            positive: 50,
-            negative: 50,
-            average: 0
-          }
-        }
-      }
-    };
+    dbActions
+      .createPresentation({
+        workspaceId: await dbActions
+          .getPersonalWorkspace(req.user._id)
+          .then(workspace => workspace._id),
+        sessionId,
+        userId: req.user._id,
+        name,
+        description,
+        settings
+      })
+      .then(presentation => {
+        /* Initialise new IO-session */
+        rooms[sessionId] = {
+          session: socketMethods.getNewSession(sessionId),
+          attendees: new Map(),
+          data: {},
+          presentation
+        };
 
-    res.status(200).json({
-      success: true,
-      session: sessionId
-    });
+        res.status(200).json({
+          success: true,
+          presentation: presentation
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          message: "There was an error creating your presentation."
+        });
+      });
+
+    // For reference:
+    //
+    // const presentationToSave = {
+    //   presentation: {
+    //     information: {
+    //       owner: req.user.name,
+    //       description: "AI in the future."
+    //     },
+    //     data: {
+    //       settings: {
+    //         isAverage: true,
+    //         threshold: 35,
+    //         description: "Faster, slower"
+    //       },
+    //       attendees: 0,
+    //       engagement: {
+    //         positive: 50,
+    //         negative: 50,
+    //         average: 0
+    //       }
+    //     }
+    //   }
+    // };
   }),
   /* Session Passthrough Route */
   router.get("/:sessionId", (req, res) => {
