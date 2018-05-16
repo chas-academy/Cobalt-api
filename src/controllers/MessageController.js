50c378b30d1197fcf51e0624fade3d6daf79d0de
Eleonore Bergqvist <eleonore296@gmail.com>
2018-05-16 20:12:21 +0200

    Nodemailer stub

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -1,4 +1,35 @@
 import express from "express";
+import { nodemailer } from 'nodemailer';
+
+const sendMail = ({from, to, subject, body}) => {
+  let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'xxx@xx.com',
+      pass: 'xxxx'
+    }
+  });
+
+  let mailOptions = {
+    from,
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    
+    console.log('Message %s sent: %s', info.messageId, info.response);
+        res.render('index');
+    });
+  });
+
+}
 
 const router = express.Router();
 
@@ -29,10 +60,17 @@ router.post("/", (req, res) => {
 
   res.json(200, {
     success: true,
-    name: req.name,
-    email: req.email,
-    message: req.message,
+    name: name,
+    email: email,
+    message: message,
   });
+
+  sendMail({
+    from: '"Cobalt" <no-reply@feed.io>',
+    to: email,
+    subject: 'Email from '+name,
+    text: message,
+  )
 });
 
 export default router;
